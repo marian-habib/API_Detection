9f57ea8ad940f6344c5c0209c0f03098e81e7c22
Marian Ashraf <marian.habib@ejada.com>
2025-03-25 13:11:06 +0200

    Detection_API

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def evaluate():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
